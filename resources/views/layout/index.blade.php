01d143f05491b7669699202600cfccdafe375938
MohdHoiruttmim <hacker.hoiruttamim@gmail.com>
2024-01-18 14:31:27 +0700

    title rename

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -10,7 +10,7 @@
   <!-- The above 6 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
   <!-- Title -->
-  <title>Monev</title>
+  <title>SIPRAS</title>
 
   <!-- Styles -->
   <link rel="preconnect" href="https://fonts.gstatic.com">
